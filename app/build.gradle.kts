79ea5ad91b257a2e174273f16a3da6c6cb05418d
febieu <febiyantiw96@gmail.com>
2023-12-22 14:15:30 +0700

    fix gradlew

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-//    implementation("com.android.tools.build:gradle:7.3.3")
+    //implementation("com.android.tools.build:gradle:8.0")
 
     // view model
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
